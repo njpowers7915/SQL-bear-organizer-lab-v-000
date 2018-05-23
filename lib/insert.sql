a12e2e95f8b4826a0f4d26b28fcc4629e4acd975
Nicholas Powers <njpowers7915@gmail.com>
2018-05-23 14:47:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 27, "M", "black", "angry", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 45, "M", "brown", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 13, "F", "black", "content", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 80, "M", "white", "happy", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 30, "F", "black", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 55, "M", "brown", "angry", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "F", "black", "happy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 18, NULL, "white", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 27, "M", "black", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 45, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 13, "F", "black", "content", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 80, "M", "white", "happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 30, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 55, "M", "brown", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "F", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 18, NULL, "white", "angry", 1);
