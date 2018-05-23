084b371fbd3b88ee48afed859127079b3151a418
Nicholas Powers <njpowers7915@gmail.com>
2018-05-23 14:46:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate", 27, "M", "black", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 27, "M", "black", "angry", 1);
